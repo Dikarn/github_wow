5ec1f6a3d628eaa8d09d3286ce28f15160b14ef1
Dikarn <68565229+Dikarn@users.noreply.github.com>
2020-07-20 23:06:11 +0300

    Add files via upload

---
+++ Xer2.py
@@ -0,0 +1,14 @@
+name= input('Ваше имя: ')
+height = float(input('Ваш рост: '))
+weight = float(input('Ваш вес: '))
+
+bmi = weight / (height ** 2)
+print(name,',''индекс вашего тела составляет:', f"{bmi:.1f}")
+
+if 1>bmi<24:
+    print('Вы Дрищ')
+elif 25>bmi<30:
+    print('Это Норма')
+else:
+    print('Ну и жирдяй')
+
